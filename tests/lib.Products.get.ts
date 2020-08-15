6511f68bd17307e7448270cf42b9e039686fd8f7
vandie <mikevdv@vandie.co.uk>
2020-08-15 17:26:16 +0100

    fix: add better commenting

--- tests/lib.Products.get.ts
+++ tests/lib.Products.get.ts
@@ -17,9 +17,11 @@ test("Test that product get works as expected", async(assert:Test) => {
     }
   ];
 
+  // We don't want to actually go to the file, so lets mock the listing function
   const productsMock = sinon.stub(listFile, "default")
     .returns(Promise.resolve(mockProducts));
   
+  // Test that a none-existing product comes back with a stock of 0
   const nonExistantProduct = await getProduct("false/01");
   assert.deepEqual(
     nonExistantProduct,
@@ -30,8 +32,13 @@ test("Test that product get works as expected", async(assert:Test) => {
     'A non existant product is returned with a stock of 0'
   );
 
+  // Test that an existing product comes back with the correct information
   const product = await getProduct('LTV719449/39/39');
-  assert.deepEqual(product, mockProducts[0], "The correct product is returned if found");
+  assert.equal(
+    product.stock,
+    mockProducts[0].stock,
+    `The correct product is returned if found (stock ${mockProducts[0].stock})`
+  );
 
 
   productsMock.restore();
